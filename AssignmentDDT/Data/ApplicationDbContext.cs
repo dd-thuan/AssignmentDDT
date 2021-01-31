cbec37aaa9c95e309b04e680cd2e9a9a29424422
Cis <thuanddgcd18677@fpt.edu.vn>
2021-01-31 18:51:00 +0700

    Add project files.

---
+++ AssignmentDDT/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AssignmentDDT.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentDDT.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Catefgory { get; set; }
+
+    }
+}
